c1dff18c29e4d46df99a39d77b2e3c949e9d2de2
jawad <shaikjawadali14@gmail.com>
2022-10-29 08:41:56 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (<>
+  <h1>My Favourite Food's</h1>
+  <ul>
+  <li>Mutton   <a href='https://en.wikipedia.org/wiki/Lamb_and_mutton'> <button type="button">Click Me! to know more</button></a></li>
+  <li>Chicken  <a href='https://en.wikipedia.org/wiki/Chicken'> <button type="button">Click Me! to know more</button></a></li>
+  <li>Fish     <a href='https://en.wikipedia.org/wiki/Fish'>   <button type="button">Click Me! to know more</button></a></li>
+  </ul></>
+    
+  );
+}
+
+export default App;
